d1bcdee3c30cab0640077b490ab1634231753c79
tsp2606-tech <tsp26062007@gmail.com>
2026-08-19 23:14:33 +0700

    feat: initialize Express server with Swagger API documentation and CORS middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import connectDB from './config/db.js';
@@ -8,6 +9,7 @@ import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 connectDB();
 

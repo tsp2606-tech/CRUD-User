8235af1be625c7e3ed5dab34dd3850978f2a705a
tsp2606-tech <tsp26062007@gmail.com>
2026-09-13 16:58:39 +0700

    feat: initialize Express server with database connection, CORS, user routes, and Swagger UI

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -42,3 +43,36 @@ app.listen(PORT, () => {
   console.log(`🚀 Server running on: http://localhost:${PORT}`);
   console.log(`📚 Swagger Docs available at: http://localhost:${PORT}/api-docs`);
 });
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+
+
+  // Vercel frontend
+  "https://fe-crud-user-thaisonpham26.vercel.app/",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Cho phép request không có Origin
+      // Ví dụ: Postman, Swagger, server-to-server
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+
+    allowedHeaders: ["Content-Type", "Authorization"],
+
+    credentials: true,
+  })
+);
